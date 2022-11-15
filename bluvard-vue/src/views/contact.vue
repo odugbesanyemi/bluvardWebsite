ab01586bd64cc0a834ac9d35912666a4d6181cc5
olobaafeezco <adelajaafeezco@gmail.com>
2022-11-15 17:53:50 +0100

    progress

--- bluvard-vue/src/views/contact.vue
+++ bluvard-vue/src/views/contact.vue
@@ -10,38 +10,39 @@
         <!-- our office -->
         <div class="bg-white">
             <div class="max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-                <h2 class=" text-gray-900 my-4 text-5xl sm:text-7xl">Our offices</h2>
-                <p class="mt-6 text-lg text-gray-500 max-w-3xl">Varius facilisi mauris sed sit. Non sed et duis dui leo,
-                    vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.</p>
+                <h2 class=" text-gray-700 my-4 text-4xl sm:text-7xl">Our offices</h2>
+                It’s amazing to have such a young, fresh, and talented new member as part of our team. 
+                <p>Hope we can together take the company to new heights. </p>
+                   
                 <div class="mt-10 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-4">
                     <div>
-                        <h3 class="text-lg font-medium text-gray-900">Los Angeles</h3>
+                        <h3 class="text-lg font-medium text-gray-900">Aruna</h3>
                         <p class="mt-2 text-base text-gray-500">
-                            <span class="block">4556 Brendan Ferry</span>
-                            <span class="block">Los Angeles, CA 90210</span>
+                            <span class="block">Haruna, Ikorodu</span>
+                            <span class="block">Lagos State.</span>
                         </p>
                     </div>
                     <div>
-                        <h3 class="text-lg font-medium text-gray-900">New York</h3>
+                        <h3 class="text-lg font-medium text-gray-900">Adamo</h3>
                         <p class="mt-2 text-base text-gray-500">
-                            <span class="block">886 Walter Streets</span>
-                            <span class="block">New York, NY 12345</span>
+                            <span class="block">Adamo, Ikorodu</span>
+                            <span class="block">Lagos State.</span>
                         </p>
                     </div>
-                    <div>
+                    <!-- <div>
                         <h3 class="text-lg font-medium text-gray-900">Toronto</h3>
                         <p class="mt-2 text-base text-gray-500">
                             <span class="block">7363 Cynthia Pass</span>
                             <span class="block">Toronto, ON N3Y 4H8</span>
                         </p>
-                    </div>
-                    <div>
+                    </div> -->
+                    <!-- <div>
                         <h3 class="text-lg font-medium text-gray-900">London</h3>
                         <p class="mt-2 text-base text-gray-500">
                             <span class="block">114 Cobble Lane</span>
                             <span class="block">London N1 2EF</span>
                         </p>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
@@ -53,28 +54,28 @@
             <div class="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
                 <div class="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
                     <div class="max-w-lg mx-auto">
-                        <p class="mt-3 text-lg leading-6 text-gray-500">Nullam risus blandit ac aliquam justo ipsum.
-                            Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
+                        <p class="mt-3 text-lg leading-6 text-gray-500">Send us a message </p>
+                          <p class="mt-3 text-lg leading-6 text-gray-500">We are online 24/7 we respond almost immediately</p>
                         <dl class="mt-8 text-base text-gray-500">
                             <div>
                                 <dt class="sr-only">Postal address</dt>
                                 <dd>
-                                    <p>742 Evergreen Terrace</p>
-                                    <p>Springfield, OR 12345</p>
+                                    <p>Aruna Ikorodu</p>
+                                    <p>Lagos State</p>
                                 </dd>
                             </div>
                             <div class="mt-6">
                                 <dt class="sr-only">Phone number</dt>
                                 <dd class="flex">
                                     <PhoneIcon class="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                                    <span class="ml-3"> +1 (555) 123-4567 </span>
+                                    <span class="ml-3"> +234 XXXXXXXX </span>
                                 </dd>
                             </div>
                             <div class="mt-3">
                                 <dt class="sr-only">Email</dt>
                                 <dd class="flex">
                                     <MailIcon class="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                                    <span class="ml-3"> support@example.com </span>
+                                    <span class="ml-3">hello@bluvard.com </span>
                                 </dd>
                             </div>
                         </dl>
