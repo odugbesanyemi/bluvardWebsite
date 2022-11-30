c3a08a7f669e3240c61f852483f8ac396f944130
olobaafeezco <adelajaafeezco@gmail.com>
2022-11-30 13:13:38 +0100

    proress

--- bluvard-vue/src/views/partnerships.vue
+++ bluvard-vue/src/views/partnerships.vue
@@ -15,7 +15,16 @@
         </div>
     </div>
 
-    <div class="bg-sky-700">
+    <div class="flex">
+        <div class="flex-1 w-64">
+            <div>
+                <h1>“If I'm looking for an organization to pursue global relief, World Concern is very high on my list. World Concern is in a position to be more nimble, and I think affect the areas that are really, truly in desperate need. There are other large, global relief organizations, but World Concern is very adept at going to the end of the road, and that's very important to me. As an investment person, I think I get the best value for my money in serving those people.”</h1>
+                   <p>MARK ANDERSON</p> 
+                   <p>Managing Principal, Tradewinds Capital Management</p> 
+
+            </div>
+
+        </div>
 
     </div>
 </template>
